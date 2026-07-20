8c53de180925f44885984e96513250283e95a401
sayani <dola2164@gmail.com>
2026-07-20 09:43:00 +0530

    Update TruckService.java

--- backend/src/main/java/com/service/TruckService.java
+++ backend/src/main/java/com/service/TruckService.java
@@ -7,7 +7,7 @@ public class TruckService {
 
     public String getBookingResponse(String source, String destination, String date) {
         return "Available: offline-ai-truck at ₹20/km for your route from " + source + " to " + destination +
-                " on " + date + ". " +
-                "Please wait up to 24 hours — we have sent your request to other registered trucks as well to find you the best price.";
+                " on " + date + ". " +"Want to book?"
+                "Please wait up to 12 hours — we have sent your request to other registered trucks as well to find you the best price.";
     }
 }
