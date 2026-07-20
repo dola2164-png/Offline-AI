1b236f3bc96ad59650d88cdd3e465b7eeaf4b04d
sayani <dola2164@gmail.com>
2026-07-20 09:46:33 +0530

    Update TruckService.java

--- backend/src/main/java/com/service/TruckService.java
+++ backend/src/main/java/com/service/TruckService.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TruckService {
-
     public String getBookingResponse(String source, String destination, String date) {
         return "Available: offline-ai-truck at ₹20/km for your route from " + source + " to " + destination +
-                " on " + date + ". " +"Want to book?"
+                " on " + date + ". " + "Want to book? " +
                 "Please wait up to 12 hours — we have sent your request to other registered trucks as well to find you the best price.";
     }
 }
